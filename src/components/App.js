78a916ac3430c68745415f24079ae153c1c7e64f
James Burns <jrburns96@aol.com>
2021-12-01 16:40:12 -0500

    working, tests not picking up.

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import {useState, useEffect} from 'react';
+function App() {
+const [loaded, setLoaded] = useState(false);
+const [image, setImage] = useState('');
+
+    useEffect(()=>{
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(resp=>resp.json())
+    .then(data=>{
+        setLoaded(true);
+        setImage(data.message)
+        console.log(data)})
+},[loaded,image])
+return(
+    <div>
+        <p>{loaded?null:"Loading..."}</p>
+        <img src={loaded?image:null} alt="A Random Dog"></img>
+    </div>
+)
+}
+
+export default App;
